7344ac607fd88a4bff1a7d805b92924f7d8f2806
SurajNaik1502 <srnaik1502@gmail.com>
2025-01-06 02:31:39 +0530

    SideBar Completed along with Routing to other links

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
+import  Link  from 'next/link'
+import { usePathname } from 'next/navigation';
+import Image from 'next/image';
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const pathname = usePathname();
 
-export default Sidebar
+  return (
+    <section style={{ color: 'white' }} className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 max-sm:hidden lg:w-[264px]">
+      <div className="flex-1 flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route || pathname.startsWith(link.route);
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={cn('flex gap-4 items-center p-4 rounded-lg justify-start', {
+                'bg-blue-1': isActive,
+              })}
+            >
+              <Image 
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
+              />
+              <p className='text-lg font-semibold max-lg:hidden'>
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Sidebar;
