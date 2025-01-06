7dd6e576a0343ccaf547f619f4c5f62f5e399608
SurajNaik1502 <srnaik1502@gmail.com>
2025-01-07 01:30:57 +0530

    Added Navbar Components

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -11,9 +11,9 @@ const Sidebar = () => {
 
   return (
     <section style={{ color: 'white' }} className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 max-sm:hidden lg:w-[264px]">
-      <div className="flex-1 flex-col gap-6">
+      <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
-          const isActive = pathname === link.route || pathname.startsWith(link.route);
+          const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
 
           return (
             <Link
