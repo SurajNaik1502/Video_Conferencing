5d83221f15bece7bfd628b6108345b3fb4f91bbc
SurajNaik1502 <srnaik1502@gmail.com>
2025-01-11 17:10:28 +0530

    Changes in Home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,29 +1,14 @@
-// import React from 'react'
-
-// const Home = () => {
-//   return (
-//     <section className='flex size-full fle-col gap-10 text-white'>
-//       <h1 className='text-3xl font-bold'>
-//         Home
-//       </h1>
-//     </section>
-    
-//   )
-// }
-
-// export default Home
-
-
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
   return (
-    <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Welcome Home</h1>
-      <p>You're successfully signed in!</p>
+    <section className='flex size-full fle-col gap-10 text-white'>
+      <h1 className='text-3xl font-bold'>
+        Home
+      </h1>
     </section>
-  );
-};
+    
+  )
+}
 
-export default Home;
-
+export default Home
