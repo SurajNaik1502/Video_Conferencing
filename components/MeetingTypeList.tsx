71795a1b8f18bdf07ad641c8e3df9e8721eeb924
SurajNaik1502 <srnaik1502@gmail.com>
2025-01-16 20:29:49 +0530

    Home Page UI completed

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,35 +5,51 @@ import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import HomeCard from './HomeCard';
+// import MeetingModal from './MeetingModal';
+// import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+// import { useUser } from '@clerk/nextjs';
+// import Loader from './Loader';
+// import { Textarea } from './ui/textarea';
+// import ReactDatePicker from 'react-datepicker';
+// import { useToast } from './ui/use-toast';
+// import { Input } from './ui/input';
 
 const MeetingTypeList = () => {
-  return (
-    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
-      <HomeCard
-        img="/icons/add-meeting.svg"
-        title="New Meeting"
-        description="Start an instant meeting"
-      />
-      <HomeCard
-        img="/icons/join-meeting.svg"
-        title="Join Meeting"
-        description="via invitation link"
-        className="bg-blue-1"
-      />
-      <HomeCard
-        img="/icons/schedule.svg"
-        title="Schedule Meeting"
-        description="Plan your meeting"
-        className="bg-purple-1"
-      />
-      <HomeCard
-        img="/icons/recordings.svg"
-        title="View Recordings"
-        description="Meeting Recordings"
-        className="bg-yellow-1"
-      />
-    </section>
-  );
+    const router = useRouter();
+    const [meetingState, setMeetingState] = useState<
+        'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
+    >(undefined);
+    return (
+        <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+            <HomeCard
+                img="/icons/add-meeting.svg"
+                title="New Meeting"
+                description="Start an instant meeting"
+                handleClick={() => setMeetingState('isInstantMeeting')}
+            />
+            <HomeCard
+                img="/icons/join-meeting.svg"
+                title="Join Meeting"
+                description="via invitation link"
+                className="bg-blue-1"
+                handleClick={() => setMeetingState('isJoiningMeeting')}
+            />
+            <HomeCard
+                img="/icons/schedule.svg"
+                title="Schedule Meeting"
+                description="Plan your meeting"
+                className="bg-purple-1"
+                handleClick={() => setMeetingState('isScheduleMeeting')}
+            />
+            <HomeCard
+                img="/icons/recordings.svg"
+                title="View Recordings"
+                description="Meeting Recordings"
+                className="bg-yellow-1"
+                handleClick={() => router.push('/recordings')}
+            />
+        </section>
+    );
 };
 
 export default MeetingTypeList;
