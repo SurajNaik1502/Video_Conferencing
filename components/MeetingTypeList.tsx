c730ba0b3d5ca8b34b07aa8b4fa23e4752f33098
SurajNaik1502 <srnaik1502@gmail.com>
2025-01-17 20:13:46 +0530

    Reusable model complete

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -24,39 +24,7 @@ const MeetingTypeList = () => {
     >(undefined);
 
     const createMeeting = async () => {
-        if (!client || !user) return;
-        try {
-            if (!values.dateTime) {
-                toast({ title: 'Please select a date and time' });
-                return;
-            }
-            const id = crypto.randomUUID();
-            const call = client.call('default', id);
-
-            if (!call) throw new Error('Failed to create meeting');
-            const startsAt =
-                values.dateTime.toISOString() || new Date(Date.now()).toISOString();
-            const description = values.description || 'Instant Meeting';
-            await call.getOrCreate({
-                data: {
-                starts_at: startsAt,
-                custom: {
-                    description,
-                },
-                },
-            });
-            setCallDetail(call);
-            if (!values.description) {
-                router.push(`/meeting/${call.id}`);
-            }
-            toast({
-                title: 'Meeting Created',
-            });
-            }
-        catch (error) {
-                console.error(error);
-                toast({ title: 'Failed to create Meeting' });
-        }
+       
     };
 
     return (
